4ebbe0c33d0142ac188be04885a081d3a94b9fe4
Connor Melnick <connorjohnmelnick@gmail.com>
2018-01-07 12:32:27 -0500

    Added get all indoors

---
+++ models/outdoors.js
@@ -0,0 +1,21 @@
+module.exports = function (sequelize, DataTypes) {
+    var Outdoors = sequelize.define("Outdoors", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dogFriendly: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    });
+    return Todo;
+};
