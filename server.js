e85c33e4492c1459bf8819bad224cc4cce19121f
Connor Melnick <connorjohnmelnick@gmail.com>
2018-01-07 13:03:50 -0500

    Fixed server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +24,8 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 // Starts the server
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
 });
