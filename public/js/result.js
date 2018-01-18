025c54ecdad165a5a0ecdee17603655fa62a1276
michellechristina <michelle@thisisbam.com>
2018-01-17 22:13:52 -0500

    added results.js with data logged

---
+++ public/js/result.js
@@ -0,0 +1,6 @@
+// This is results from the api search. this is 5 google places that are not known ruff spots.
+var googlePlaces = JSON.parse(localStorage.getItem('data'));
+
+console.log(googlePlaces);
+
+$('#places').html(JSON.stringify(googlePlaces));
