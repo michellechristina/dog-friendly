aaae01b66bf9a75f0e6a996c4e1f167189b240b7
michellechristina <michelle@thisisbam.com>
2018-01-19 21:05:29 -0500

    added form to index.html, added get request to index.js, added dummy images

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('select').material_select();
-  });
+});
 
+// bringing variables back into scope so the onclick event can access them
 var latitude = "";
 var longitude = "";
 
 
-// get geolocation upon page load
+// get geolocation upon page load, (used for the NEAR ME search)
 var geolocate = $("#geolocate");
 
 function getLocation() {
@@ -16,45 +17,46 @@ function getLocation() {
         alert("Geolocation is not supported by this browser.");
     }
 }
+
 function showPosition(position) {
-    latitude= position.coords.latitude;
-    longitude=position.coords.longitude; 
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
     console.log("INDEX Latitude: " + latitude + "\n INDEX Longitude: " + longitude);
 }
 
 getLocation()
 
-
+// event handler for the NEAR ME submit button
 $("#localSubmit").on("click", function (event) {
-      // Prevent form from submitting
-      event.preventDefault();
-    
+    // Prevent form from submitting
+    event.preventDefault();
+
     var place = $("#typeOfPlace option:selected").val();
     var newPlace = place.split(" ").join("+");
+    console.log("THIS IS THE PLACE YOU ARE LOOKING FOR");
     console.log(newPlace);
 
-  // This goes server side, where google places api is queried & results are returned.
+    // This request goes server side, where google places api is queried & results are returned.
+    $.ajax({
+        method: "get",
+        url: `/api/places/${newPlace}/${latitude}/${longitude}`,
+        success: function (response) {
+            console.log(response);
+console.log("THIS IS THE URL YOU ARE LOOKING FOR");
+// console.log(url);
 
-  $.ajax({
-    method: "get",
-    url: `/api/places/${newPlace}/${latitude}/${longitude}`,
-    success: function (response) {
-        console.log(response);
+            /// store response in local storage, it will have data you need to build the 2 views
+            localStorage.setItem('data', JSON.stringify(response.data));
+            /// if response.ruffSpots then redirect to browse.html
+            if (response.ruffSpots) {
+                window.location = "browse.html";
+            }
+            /// else redirect to result.html
+            else {
+                window.location = "result.html"
+            };
 
-
-        /// store response in local storage, it will have data you need to build the 2 views
-        localStorage.setItem('data', JSON.stringify(response.data));
-        /// if response.ruffSpots then redirect to browse.html
-        if (response.ruffSpots) {
-            window.location = "browse.html";
-        }
-        /// else redirect to result.html
-        else {
-            window.location = "result.html"
-        };
-
-    } // this ends the callback function
-})
-
+        } 
+    })
 
 })
