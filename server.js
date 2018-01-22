10bb7c22d328bc6f519f6115a92257cea3cf40bf
vag-tsdc <vagtsdc@gmail.com>
2018-01-22 18:38:15 -0500

    first heroku commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8000;
 
 // Requiring our models for syncing
 var db = require("./models");
