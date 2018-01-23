e8d405e3f177be41a393592231cfc1aac53a5104
michellechristina <michelle@thisisbam.com>
2018-01-23 12:02:46 -0500

    test on api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -31,7 +31,7 @@ module.exports = function (app) {
   app.put("/api/reviews/:review/:friendly_rating", function (req, res) {
     //Add the new review to the review DB
     db.reviews.create({
-      place_id: req.params.placeID,
+      // place_id: req.params.placeID,
       review: req.params.review,
       friendly_rating: req.params.friendly_rating,
     }).then(function (newReview) {
